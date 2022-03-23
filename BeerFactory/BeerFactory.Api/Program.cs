a260658eda24dd739242dbd916392919b4f4d943
Martin Othamar <Martin.Othamar@dnv.com>
2022-03-23 10:18:46 +0100

    add api stub

---
+++ BeerFactory/BeerFactory.Api/Program.cs
@@ -0,0 +1,26 @@
+using Hopster;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapPost("/bottles", ([FromBody] Bottle bottle) =>
+{
+    Console.WriteLine("Test");
+})
+.WithName("Bottles");
+
+app.Run();

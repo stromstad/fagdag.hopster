70b9d2b89392fa18bd502bab919a69a7128d6e98
Jonas Strømstad <jonas.stromstad@innovasjonnorge.no>
2022-03-23 09:52:16 +0100

    Initial comit

---
+++ BeerFactory/BeerFactory/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var client = new Hopster.Client("http://hopster.m07039.clients.dev.nrk.no", new HttpClient());
+
